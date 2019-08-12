00d3912a7fe359bd53c19034146552075a3f1feb
Zhasmina Mihaylova <jasmina.mihaylova@gmail.com>
2019-08-12 15:52:07 +0300

    Populated page header with links and added favicon

--- src/components/common/PageHeader.vue
+++ src/components/common/PageHeader.vue
@@ -1,22 +1,48 @@
 <template>
+  <nav>
     <v-app-bar app dark class="brown">
-      <v-toolbar-title class="headline">
-        <span class="mr-1">Mocha</span>
-        <span class="font-weight-light">Rent-a-car</span>
-      </v-toolbar-title>
+      <router-link to="/"
+      tag="v-toolbar-title" class="headline toolbar-title" :style="{ cursor: 'pointer'}">
+          <span class="mr-1">Mocha</span>
+          <span class="font-weight-light">Rent-a-car</span>
+      </router-link>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Log in</span>
-      </v-btn>
+      <template v-if="isAuthorized">
+        <v-btn text to="/carcreate">
+          <span class="mx-1">Create Car</span>
+        </v-btn>
+        <v-btn text to="/logout">
+          <span class="mx-1">Log out</span>
+        </v-btn>
+      </template>
+      <template v-else>
+        <v-btn text to="/about">
+          <span class="mx-1">About</span>
+        </v-btn>
+        <v-btn text to="/contacts">
+          <span class="mx-1">Contacts</span>
+        </v-btn>
+        <v-btn text to="/login">
+          <span class="mx-1">Log in</span>
+        </v-btn>
+        <v-btn text to="/signup">
+          <span class="mx-1">Sign Up</span>
+        </v-btn>
+      </template>
     </v-app-bar>
+  </nav>
 </template>
 
 <script>
 export default {
-
+data() {
+  return {
+    isAuthorized: false
+  }
+}
 };
 </script>
+
+<style scoped>
+
+</style>
--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="home">
-        
+        <h1 class="display-3 text-center brown--text text-capitalize mt-3">
+            <v-btn fab class="red darken-1 white--text ma-3">
+                <v-icon small>fas fa-coffee</v-icon>
+            </v-btn>Be in a transport of delight
+        </h1>
+        <v-btn small class="red darken-1 white--text mx-2 text-uppercase">Book</v-btn>
+        <v-btn small class="teal white--text text-uppercase">
+            <v-icon small left>fas fa-car</v-icon>
+            <span>Info</span>
+        </v-btn>
     </div>
 </template>
 
