54dcd7cdc7e6897629e845728a3f2f9d101221bf
Zhasmina Mihaylova <jasmina.mihaylova@gmail.com>
2019-08-17 14:36:33 +0300

    added validation and functionality to Login Form

--- src/components/authentication/Login.vue
+++ src/components/authentication/Login.vue
@@ -1,43 +1,106 @@
 <template>
-    <v-dialog v-model="dialog" persistent max-width="600px">
+    <v-dialog v-model="dialog" persistent max-width="500px">
       <template v-slot:activator="{ on }">
             <v-btn text v-on="on">
-                <span class="mx-1">Login</span>
+                <span class="mx-1">Log In</span>
             </v-btn>
       </template>
-      <v-card>
+      <v-card ref="card">
         <v-card-title>
-          <span class="headline">Login</span>
+          <span class="headline">Log In</span>
         </v-card-title>
         <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12">
-                <v-text-field color="brown" label="Email*" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field color="brown" label="Password*" type="password" required></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+            class="px-3"
+          >
+            <v-text-field
+              color="brown"
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              color="brown"
+              type="password"
+              v-model="password"
+              :rules="passwordRules"
+              label="Password"
+              required
+            ></v-text-field>
+
+            <v-layout justify-end>
+            <v-btn
+              dark
+              color="brown"
+              class="mr-4 mt-5"
+              @click="cancel"
+            >
+              Cancel
+            </v-btn>
+
+            <v-btn
+              :disabled="!valid"
+              class="teal white--text mt-5"
+              @click="login"
+            >
+              Log In
+            </v-btn>
+            </v-layout>
+          </v-form>
         </v-card-text>
-        <!-- <v-layout justify-center> -->
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="brown" text @click="dialog = false">Cancel</v-btn>
-          <v-btn color="brown" text @click="dialog = false">Login</v-btn>
-        </v-card-actions>
-        <!-- </v-layout> -->
       </v-card>
     </v-dialog>
 </template>
 
 
 <script>
+import { db, auth, functions } from '@/fb'
+
   export default {
     data: () => ({
       dialog: false,
+      valid: true,
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      password: '',
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(v) || 'Password must be at least 8 characters long and include at least one letter and one number',
+      ],
     }),
+
+    methods: {
+      cancel () {
+        this.dialog = false;
+        this.$refs.form.reset();
+      },
+      login () {
+        if (!this.$refs.form.validate()) {
+          this.snackbar = true;
+        } else {
+              // get user info
+          const email = this.email;
+          const password = this.password;
+
+          auth.signInWithEmailAndPassword(email, password)
+              .then(cred => {
+                  console.log(cred.user);
+                  // close the login modal and reset the form
+                  this.dialog = false;
+                  this.$refs.form.reset();
+              }).catch(err => {
+                  console.log(err.message);
+              });
+        }
+      }
+    },
   }
 </script>
