726d3a9cb93578378876f331233a34760efd8cd6
Zhasmina Mihaylova <jasmina.mihaylova@gmail.com>
2019-08-17 14:40:27 +0300

    added Logout functionality

---
+++ src/components/authentication/Logout.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-btn text @click="logout">
+        <span class="mx-1">Log out</span>
+    </v-btn>
+</template>
+
+<script>
+import { auth } from '@/fb'
+export default {
+  methods: {
+    logout() {
+      console.log(auth.signOut());
+    }
+  }
+};
+</script>
