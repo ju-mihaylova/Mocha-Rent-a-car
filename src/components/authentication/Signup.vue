30f6f68e9cf9a5a2839d6f40c9a34da2d4769688
Zhasmina Mihaylova <jasmina.mihaylova@gmail.com>
2019-08-17 11:42:22 +0300

    added validation and functionality to Signup Form

--- src/components/authentication/Signup.vue
+++ src/components/authentication/Signup.vue
@@ -1,49 +1,124 @@
 <template>
-    <v-dialog v-model="dialog" persistent max-width="600px">
+    <v-dialog v-model="dialog" persistent max-width="500px">
       <template v-slot:activator="{ on }">
             <v-btn text v-on="on">
-                <span class="mx-1">Sign up</span>
+                <span class="mx-1">Sign Up</span>
             </v-btn>
       </template>
-      <v-card>
+      <v-card ref="card">
         <v-card-title>
-          <span class="headline">Sign up</span>
+          <span class="headline">Sign Up</span>
         </v-card-title>
         <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12">
-                <v-text-field color="brown" label="Name*" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field color="brown" label="Email*" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field color="brown" label="Password*" type="password" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field color="brown" label="Confirm Password*" type="password" required></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+            class="px-3"
+          >
+            <v-text-field
+              color="brown"
+              v-model="name"
+              :counter="50"
+              :rules="nameRules"
+              label="Name"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              color="brown"
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              color="brown"
+              type="password"
+              v-model="password"
+              :rules="passwordRules"
+              label="Password"
+              required
+            ></v-text-field>
+
+            <v-layout justify-end>
+            <v-btn
+              dark
+              color="brown"
+              class="mr-4 mt-5"
+              @click="cancel"
+            >
+              Cancel
+            </v-btn>
+
+            <v-btn
+              :disabled="!valid"
+              class="teal white--text mt-5"
+              @click="submit"
+            >
+              Submit
+            </v-btn>
+            </v-layout>
+          </v-form>
         </v-card-text>
-        <!-- <v-layout justify-center> -->
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="brown" text @click="dialog = false">Cancel</v-btn>
-          <v-btn color="brown" text @click="dialog = false">Sign up</v-btn>
-        </v-card-actions>
-        <!-- </v-layout> -->
       </v-card>
     </v-dialog>
 </template>
 
 
 <script>
-  export default {
+import { db, auth, functions } from '@/fb'
+
+export default {
     data: () => ({
       dialog: false,
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 50) || 'Name must be less than 50 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      password: '',
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(v) || 'Password must be at least 8 characters long and include at least one letter and one number',
+      ],
     }),
+
+    methods: {
+      cancel () {
+        this.dialog = false;
+        this.$refs.form.reset();
+      },
+      submit () {
+        if (!this.$refs.form.validate()) {
+          this.snackbar = true;
+        } else {
+        // get user info
+        const name = this.name;
+        const email = this.email;
+        const password = this.password;
+
+        // sign up the user
+        auth.createUserWithEmailAndPassword(email, password)
+            .then(cred => {
+                return db.collection('users').doc(cred.user.uid).set({
+                    name
+                });
+            }).then(() => {
+                this.dialog = false;
+                this.$refs.form.reset();
+            }).catch(err => {
+                console.log(err.message);
+            });
+        }
+      }
+    },
   }
 </script>
