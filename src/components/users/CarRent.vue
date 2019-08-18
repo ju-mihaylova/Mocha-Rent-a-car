41b164d99838a796bcff5ec0574696490b029907
Zhasmina Mihaylova <jasmina.mihaylova@gmail.com>
2019-08-18 14:41:53 +0300

    added total price calculation to CarRent Form

--- src/components/users/CarRent.vue
+++ src/components/users/CarRent.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-dialog v-model="dialog" persistent max-width="500px">
+    <v-dialog v-model="dialog" persistent max-width="550px">
         <template v-slot:activator="{ on }">
             <v-btn 
                 text
@@ -20,9 +20,15 @@
             class="px-3"
           >
             <v-row>
-              <v-col cols="12">
+              <v-col cols="12" sm="6" md="8">
                 <span class="display-1 blue-grey--text text--darken-1 font-weight-medium"  
                 >{{ carBrand }} {{ carModel }}</span>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">                
+                <span v-if="totalPrice > 0" class="title blue-grey--text text--darken-1 font-weight-medium"  
+                ><strong>Total:</strong> {{ totalPrice }} EUR</span>
+                <span v-else class="title blue-grey--text text--darken-1 font-weight-medium"  
+                ><strong>Price:</strong> {{ carPrice }} EUR</span>
               </v-col>
             </v-row>
 
@@ -59,7 +65,7 @@
                     <v-date-picker v-model="dateStart" no-title scrollable color="brown">
                     <v-spacer></v-spacer>
                     <v-btn text color="brown" @click="menuStart = false">Cancel</v-btn>
-                    <v-btn text color="brown" @click="$refs.menuStart.save(dateStart)">OK</v-btn>
+                    <v-btn text color="brown" @click="onDateStartClick">OK</v-btn>
                     </v-date-picker>
                 </v-menu>
                 </v-col>
@@ -88,7 +94,7 @@
                     <v-date-picker v-model="dateEnd" no-title scrollable color="brown">
                     <v-spacer></v-spacer>
                     <v-btn text color="brown" @click="menuEnd = false">Cancel</v-btn>
-                    <v-btn text color="brown" @click="$refs.menuEnd.save(dateEnd)">OK</v-btn>
+                    <v-btn text color="brown" @click="onDateEndClick">OK</v-btn>
                     </v-date-picker>
                 </v-menu>
                 </v-col>
@@ -119,40 +125,78 @@
 </template>
 
 <script>
-  export default {
-    props: [
-      'carBrand',
-      'carModel',
-      'carId'
-    ],
-    data: () => ({
-      dialog: false,
-      valid: true,
-      dateStart: new Date().toISOString().substr(0, 10),
-      dateEnd: new Date().toISOString().substr(0, 10),
-      menuStart: false,
-      menuEnd: false,
-      license: '',
-      licenseRules: [
-        v => !!v || 'License is required',
-        v => /^\d{9}$/.test(v) || 'License must be valid',
+export default {
+  props: [
+    'carBrand',
+    'carModel',
+    'carId',
+    'carPrice'
+  ],
+  data: () => ({
+    dialog: false,
+    valid: true,
+    dateStart: new Date().toISOString().substr(0, 10),
+    dateEnd: new Date().toISOString().substr(0, 10),
+    menuStart: false,
+    menuEnd: false,
+    license: '',
+    licenseRules: [
+      v => !!v || 'License is required',
+      v => /^\d{9}$/.test(v) || 'License must be valid',
 
-      ]
-    }),
+    ]
+  }),
+  computed: {
+    totalPrice() {
+      let start = this.dateStart;
+      let end = this.dateEnd;
+      if(typeof start ===  'undefined' || typeof end === 'undefined') {
+        return;
+      }
 
-    methods: {
-      cancel () {
+      let startDateParts = start.split('-');
+      let jsStartDate = new Date(startDateParts[0], startDateParts[1] - 1, startDateParts[2].substr(0,2));
+      
+      let endDateParts = end.split('-');
+      let jsEndDate = new Date(endDateParts[0], endDateParts[1] - 1, endDateParts[2].substr(0,2));
+
+      let diff = jsEndDate - jsStartDate;
+      const divideBy = 86400000;
+
+      let daysBooked = Math.floor(diff/divideBy);
+
+      return daysBooked * this.carPrice;
+    }
+  },
+
+  methods: {
+    cancel () {
+      this.dialog = false;
+      this.$refs.form.reset();
+      this.dateStart = new Date().toISOString().substr(0, 10);
+      this.dateEnd = new Date().toISOString().substr(0, 10);
+    },
+    book () {
+      if (!this.$refs.form.validate()) {
+        this.snackbar = true;
+      } else {
+        
+
+
+        // close dialog
         this.dialog = false;
+        // reset form
         this.$refs.form.reset();
-      },
-      book () {
-        if (!this.$refs.form.validate()) {
-          this.snackbar = true;
-        } else {
-          this.dialog = false;
-          this.$refs.form.reset();
-        }
+        this.dateStart = new Date().toISOString().substr(0, 10);
+        this.dateEnd = new Date().toISOString().substr(0, 10);
       }
     },
-  }
+    onDateStartClick () {
+      this.$refs.menuStart.save(this.dateStart);
+    },
+    onDateEndClick () {
+      this.$refs.menuEnd.save(this.dateEnd);
+    }
+  },
+}
 </script>
