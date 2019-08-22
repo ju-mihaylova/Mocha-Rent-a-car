8af6bd895998aed2e49f43321f4fd292b337799a
Zhasmina Mihaylova <jasmina.mihaylova@gmail.com>
2019-08-22 18:50:07 +0300

    added info to about page

--- src/components/common/About.vue
+++ src/components/common/About.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="about">
-        <h1 class="display-3 text-center brown--text text-capitalize mt-3">
-            <v-btn fab class="red darken-1 white--text ma-3">
-                <v-icon small>fas fa-coffee</v-icon>
-            </v-btn>Be in a transport of delight
-            
-        </h1>       
+        <v-container>
+        <h1 class="display-3 text-center grey--text text--darken-2 text-capitalize my-3">
+            Be in a transport of delight
+        </h1>
+        <h4 class="teal--text">About Mocha Rent-A-Car</h4>
+        <p>Mocha Rent-A-Car is a young Bulgarian car rental company established in 2010. Currently we have twelve different vehicles to choose from ranging from an economy class Citroen C 3 to a C-Class Tesla . All cars are 90% brand new or up to 3 years old. We provide nationwide car rental service deliveries in every city and resort in Bulgaria.</p>
+        <h4 class="teal--text">Our Mission</h4>
+        <p>Our main goal is to provide high quality professional car rental service to our customers. We pay particular attention to customer satisfaction. We keep improving the quality of our service by providing technical innovations, new cars and competitive prices.</p>
+        <h4 class="teal--text">Payment Methods</h4>
+        <p>Payment can be made in our office before car pick-up.</p>
+        </v-container> 
+    
     </div>
 </template>
 
