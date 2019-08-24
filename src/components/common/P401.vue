afa275892e2aafacbafdb0af1fe71671e8de0679
Zhasmina Mihaylova <jasmina.mihaylova@gmail.com>
2019-08-24 14:41:10 +0300

    added 401 and 404 error pages

---
+++ src/components/common/P401.vue
@@ -0,0 +1,12 @@
+<template>
+<v-container class="my-5 text-center">
+    <h1 class="display-3 mb-3">401 Unauthorized Error</h1>
+    <p class="subheading">You don't have permissions to access this page.</p>
+</v-container>
+</template>
+
+<script>
+export default {
+
+};
+</script>
---
+++ src/components/common/P404.vue
@@ -0,0 +1,12 @@
+<template>
+<v-container class="my-5 text-center">
+    <h1 class="display-3 mb-3">404 Not Found</h1>
+    <p class="subheading">The page you are trying to access does not exist.</p>
+</v-container>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
